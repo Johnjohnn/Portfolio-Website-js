2ce344d80965ba8d1dc789fa8eccc66dbe82e031
johnjohnn <conde2010@aol.com>
2022-07-25 19:48:35 -0400

    finished the loader

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,6 @@
+import gsap from "gsap";
 const bar = document.querySelector(".loading__bar--inner");
-const number = document.querySelector(".number")
+const number = document.querySelector(".loading__counter--number")
 let c = 0;
 
 let barInterval = setInterval(() => {
@@ -8,16 +9,48 @@ let barInterval = setInterval(() => {
  if (c===100){
     clearInterval(barInterval)
  }  
-},50);
+},20);
 
 
 let i = 0;
 
 let numberInterval = setInterval(() => {
-number.innerHTML = i+"%"
+number.textContent = i+"%"
 i++;
 
 if (i===101) {
     clearInterval(numberInterval)
+    gsap.to(".loading__bar",{
+        duration:5,
+        rotate: "90deg", 
+        left: "1000%",
+    });
+    gsap.to(".loading__text,.loading__counter",{
+        duration:1,
+        opacity:0,
+    });
+    gsap.to(".loading__box",{
+        duration:1,
+       height: "500px",
+       borderRadius: "50%"
+    });
+    gsap.to(".loading_svg",{
+        duration:10,
+       opacity: "1",
+       rotate: "360deg"
+    });
+    gsap.to(".loading__box",{
+        duration:1,
+       delay: 2,
+       border: "none"
+    });
+    gsap.to(".loading",{
+       delay: 2,
+       duration:2,
+       background: "transparent",
+       opacity: 0,
+    });
 }
-},50)
+},20)
+
+
