ebd563bcd8a729b2e0a218c1f61ad216c34ea4a8
johnjohnn <conde2010@aol.com>
2022-08-27 19:33:22 -0400

    adding the social media section

--- dist/app.56908c73.js
+++ dist/app.56908c73.js
@@ -18738,7 +18738,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55176" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49921" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
