ce0db0fc24f0cb0d8c071275870532fb6caf5732
johnjohnn <conde2010@aol.com>
2022-08-28 20:37:53 -0400

    almost done with background

--- src/assets/js/threebg.js
+++ src/assets/js/threebg.js
@@ -1,3 +1,4 @@
+import { Thumbs } from "swiper";
 import * as THREE from "three";
 import images from "./images"
 const loader = new THREE.TextureLoader();
@@ -16,18 +17,28 @@ window.addEventListener("resize", ()=> {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 })
-const geometry = new THREE.PlaneGeometry(18, 10);
+const geometry = new THREE.PlaneGeometry(18, 10, 15, 9);
 const material = new THREE.MeshBasicMaterial({
-    color:0xff0000,
-    map:loader.load(images.bg1)
-
+    //color:0xff0000,
+    map:loader.load(images.bg2),
 });
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
 camera.position.z = 5;
 
+const count = geometry.attributes.position.count;
+const clock = new THREE.Clock();
 function animate(){
+    const time = clock.getElapsedTime();
+    for( let i = 0; i < count; i++) {
+     const x = geometry.attributes.position.getX(i);
+     const y = geometry.attributes.position.getY(i);
+
+     geometry.attributes.position.setZ(i, 0.2 * Math.sin(x * time * .6));
+     geometry.computeVertexNormals();
+     geometry.attributes.position.needsUpdate =  true;
+    }
     requestAnimationFrame(animate);
     //mesh.rotation.x += 0.01;
     //mesh.rotation.y += 0.01;
