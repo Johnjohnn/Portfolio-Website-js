e1f74fee06436e9bf4c41a22b3ea5fec6d78b853
johnjohnn <conde2010@aol.com>
2022-08-30 19:48:27 -0400

    finished fixing media queries for mobile

--- dist/threebg.0bc442f5.js
+++ dist/threebg.0bc442f5.js
@@ -35963,7 +35963,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49163" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55121" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
